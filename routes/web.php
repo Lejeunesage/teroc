2e9cd0603d09f731b08a024b8a06f2d5f021d5a8
Lejeunesage <geoffroyotegbeye@gmail.com>
2023-02-08 22:01:20 +0100

    commentaires

--- routes/web.php
+++ routes/web.php
@@ -63,3 +63,120 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+
+
+/*/**
+ 
+//list//
+Route::get('activities', [ActivityController::class, "index"])->name('activities')->middleware('auth');
+Route::get('activity/{id}', [ActivityController::class, "show"])->name('user.show')->middleware('auth');
+//create//
+Route::get('activity', [ActivityController::class, "create"])->name('activity.create')->middleware('auth');
+Route::post('activity', [ActivityController::class, "store"])->name('activity.store')->middleware('auth');
+//update//
+Route::get('activities/{id}', [ActivityController::class, "edit"])->name('activity.edit')->middleware('auth');
+Route::put('activity/{id}', [ActivityController::class, "update"])->name('activity.update')->middleware('auth');
+//delete//
+Route::delete('activity/{id}', [ActivityController::class, "destroy"])->name('activity.destroy')->middleware('auth');
+*/
+
+
+/*
+
+
+            
+        <div class="shadow-sm rounded-lg my-3">
+                    <div v-if="$page.props.flash.success" class="font-medium text-white bg-green-600 py-3 px-6">{{
+                        $page.props.flash.success
+                    }}</div>
+                    <div v-if="$page.props.flash.error" class="font-medium text-white bg-red-600 py-3 px-6">{{
+                        $page.props.flash.error
+                    }}</div>
+                </div>
+        <Head title="Créer une campagne" />
+
+
+        <h1>Créer une nouvelle campagne</h1>
+
+        <form @submit.prevent="submit" class="max-w-full pt-6" >
+
+            <div class="flex  justify-center  gap-9">
+                <div class="w-full flex flex-col gap-2">
+                    <div class="w-md">
+                            <InputLabel for="description" value="Description" />
+
+                            <TextInput id="description" type="text" class="mt-1 block w-full" v-model="form.description" required
+                                autofocus autocomplete="description" />
+
+                            <InputError class="mt-2" :message="form.errors.description" />
+                        </div>
+
+                        <div class="m">
+                            <InputLabel for="groupe" value="Groupe" />
+
+                            <TextInput id="groupe" type="text" class="mt-1 block w-full" v-model="form.groupe" required
+                                autocomplete="groupe" />
+
+                            <InputError class="mt-2" :message="form.errors.groupe" />
+                        </div>
+
+                        <div class="m">
+                            <InputLabel for="status" value="Status" />
+
+                            <select autocomplete="status" v-model="form.status" id="status" class=" border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                            <option selected value="ACTIVED">ACTIVED</option>
+                            <option value="PAUSED">PAUSED</option>
+                            <option value="CLOSED">CLOSED</option>
+
+                            </select>
+
+
+                            <InputError class="mt-2" :message="form.errors.status" />
+                        </div>
+
+
+
+                </div>
+
+
+            <div class="w-full flex flex-col gap-2">
+                    <div class="m">
+                        <InputLabel for="destination" value="Destination" />
+
+                        <TextInput id="destination" type="text" class="mt-1 block w-full" v-model="form.destination" required
+                            autocomplete="destination" />
+
+                        <InputError class="mt-2" :message="form.errors.destination" />
+                    </div>
+
+                    <div class="m">
+                        <InputLabel for="options" value="Option" />
+
+                         <select autocomplete="options" v-model="form.options" id="options" class=" border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                            <option  selected  value="option1">Option 1</option>
+                            <option  value="option2">Option 2</option>
+                            <option  value="option3">Option 3</option>
+
+                         </select>
+
+                        <InputError class="mt-2" :message="form.errors.option" />
+                    </div>
+
+
+            </div>
+            </div>
+
+           <div class="flex justify-end">
+            <PrimaryButton class="ml-4 my-6 "  :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
+                Enrégistrer
+            </PrimaryButton>
+           </div>
+
+        </form>
+
+
+
+
+
+ */
